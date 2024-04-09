3eee51257e91545c12f178b78845942ef2c3e38d
Andrew Skow <skovorodnikov11@gmail.com>
2024-04-09 11:08:39 +0300

    good migrations

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import (CreateAPIView, RetrieveAPIView,
+                                     UpdateAPIView, DestroyAPIView,
+                                     ListAPIView)
 
-# Create your views here.
+
+from materials.models import Course
+from materials.serializers import CourseSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class LessonCreateAPIViewSet(CreateAPIView):
+    pass
+
+class LessonListAPIViewSet(ListAPIView):
+    pass
+
+class LessonRetrieveAPIViewSet(RetrieveAPIView):
+    pass
+
+class LessonUpdateAPIViewSet(UpdateAPIView):
+    pass
+
+class LessonDestroyAPIViewSet(DestroyAPIView)
+    pass
