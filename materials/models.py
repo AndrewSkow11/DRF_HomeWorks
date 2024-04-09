da1e66a614a690a273f157ad953a83c0e57feec1
Andrew Skow <skovorodnikov11@gmail.com>
2024-04-09 10:49:25 +0300

    good migrations

--- materials/models.py
+++ materials/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Course(models.Model):
@@ -7,6 +8,13 @@ class Course(models.Model):
     preview = models.ImageField(upload_to='course_preview',
                                 verbose_name='превью', null=True, blank=True)
     description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
 
 
 class Lesson(models.Model):
@@ -18,3 +26,10 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='lesson_preview',
                                 verbose_name='превью', null=True, blank=True)
     video = models.FileField(upload_to='video_lesson', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "урок"
+        verbose_name_plural = 'уроки'
