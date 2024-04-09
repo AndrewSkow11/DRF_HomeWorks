81803aa564df1717c65ace9434c7165560d8694b
Andrew Skow <skovorodnikov11@gmail.com>
2024-04-09 17:14:10 +0300

    models, urls, views - ok

--- materials/urls.py
+++ materials/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from materials.views import *
+from rest_framework import routers
 
+router = routers.SimpleRouter()
+router.register('course', CourseAPIViewSet)
 
 urlpatterns = [
-    path('', LessonListAPIView.as_view(), name='lesson_list'),
-    path('create/', LessonCreateAPIView.as_view(), name='lesson_create'),
-    path('update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
-    path('detail/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_delete'),
-    path('delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_destroy'),
-]
+                  path('', LessonListAPIView.as_view(), name='lesson_list'),
+                  path('create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+                  path('update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+                  path('detail/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_delete'),
+                  path('delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_destroy'),
+              ] + router.urls
