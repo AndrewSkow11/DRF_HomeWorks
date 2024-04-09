b02399d1d495c826b2bab42771bb116b6c6cc494
Andrew Skow <skovorodnikov11@gmail.com>
2024-04-09 18:15:05 +0300

    course, lessons urls - ok

--- materials/urls.py
+++ materials/urls.py
@@ -7,8 +7,8 @@ router.register('course', CourseAPIViewSet)
 
 urlpatterns = [
                   path('lessons/', LessonListAPIView.as_view(), name='lesson_list'),
-                  path('lesson_create/', LessonCreateAPIView.as_view(), name='lesson_create'),
-                  path('lesson_update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
-                  path('lesson_detail/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_delete'),
-                  path('lesson_delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_destroy'),
+                  path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+                  path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+                  path('lesson/detail/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_delete'),
+                  path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_destroy'),
 ] + router.urls
