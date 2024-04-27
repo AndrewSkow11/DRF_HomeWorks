6ed9ad1c7568d0b8d5de2fc5478d3388817ea0d7
Andrew Skow <skovorodnikov11@gmail.com>
2024-04-27 16:51:27 +0300

    26_2 celary-beat

--- config/settings.py
+++ config/settings.py
@@ -47,9 +47,8 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework_simplejwt',
     'drf_yasg',
-    # ALL YOUR APPS
     'drf_spectacular',
-    # 'drf_spectacular_sidecar',  # required for Django collectstatic discovery
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -171,11 +170,8 @@ STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
 
 # Настройки для Celery
 
-# URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://localhost:6379' # Например, Redis, который по умолчанию работает на порту 6379
-
-# URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = "Australia/Tasmania"
@@ -185,3 +181,10 @@ CELERY_TASK_TRACK_STARTED = True
 
 # Максимальное время на выполнение задачи
 CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BEAT_SCHEDULE = {
+    'task-name': {
+        'task': 'myapp.tasks.my_task',  # Путь к задаче
+        'schedule': timedelta(minutes=10),  # Расписание выполнения задачи (например, каждые 10 минут)
+    },
+}
